1ef0a01f814a84eb4622cc65f3e7f220239d8108
LightsOnHudson <bshaver@MBAIR-TDL.home.lan>
2017-07-22 07:30:02 -0600

    First Submit

---
+++ functions.inc.php
@@ -0,0 +1,173 @@
+<?php
+function sendTCP($IP, $PORT, $cmd) {
+	
+	
+/* Create a TCP/IP socket. */
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    logEntry("socket_create() failed: reason: " . socket_strerror(socket_last_error()));
+} else {
+   logEntry("TCPIP Socket Created");
+}
+
+
+$result = socket_connect($socket, $IP, $PORT);
+if ($result === false) {
+    logEntry("socket_connect() failed. Reason: ($result) " . socket_strerror(socket_last_error($socket)));
+} else {
+    logEntry("TCPIP CONNECTED");
+}
+
+
+socket_write($socket, $cmd, strlen($cmd));
+
+
+logEntry("Reading response");
+while ($out = socket_read($socket, 2048)) {
+    logEntry($out);
+}
+
+logEntry("Closing socket...");
+socket_close($socket);
+logEntry("OK");
+
+}
+function hex_dump($data, $newline="\n")
+{
+  static $from = '';
+  static $to = '';
+
+  static $width = 16; # number of bytes per line
+
+  static $pad = '.'; # padding for non-visible characters
+
+  if ($from==='')
+  {
+    for ($i=0; $i<=0xFF; $i++)
+    {
+      $from .= chr($i);
+      $to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+    }
+  }
+
+  $hex = str_split(bin2hex($data), $width*2);
+  $chars = str_split(strtr($data, $from, $to), $width);
+
+$HEX_OUT ="";
+  $offset = 0;
+  foreach ($hex as $i => $line)
+  {
+    $HEX_OUT.= sprintf('%6X',$offset).' : '.implode(' ', str_split($line,2)) . ' [' . $chars[$i] . ']';
+    $offset += $width;
+  }
+return $HEX_OUT;
+}
+
+function decode_code($code)
+{
+    return preg_replace_callback('@\\\(x)?([0-9a-f]{2,3})@',
+        function ($m) {
+            if ($m[1]) {
+                $hex = substr($m[2], 0, 2);
+                $unhex = chr(hexdec($hex));
+		echo "UNHEX: ".$unhex;
+                if (strlen($m[2]) > 2) {
+                    $unhex .= substr($m[2], 2);
+                }
+                return $unhex;
+            } else {
+                return chr(octdec($m[2]));
+            }
+        }, $code);
+}
+
+
+function logEntry($data) {
+
+	global $logFile,$myPid,$callBackPid;
+	
+	if($callBackPid != "") {
+		$data = $_SERVER['PHP_SELF']." : [".$callBackPid.":".$myPid."] ".$data;
+	} else { 
+	
+		$data = $_SERVER['PHP_SELF']." : [".$myPid."] ".$data;
+	}
+	$logWrite= fopen($logFile, "a") or die("Unable to open file!");
+	fwrite($logWrite, date('Y-m-d h:i:s A',time()).": ".$data."\n");
+	fclose($logWrite);
+}
+
+
+function escapeshellarg_special($file) {
+	return "'" . str_replace("'", "'\"'\"'", $file) . "'";
+}
+
+
+function processCallback($argv) {
+
+	global $DEBUG,$pluginName;
+	
+	
+	if($DEBUG)
+		print_r($argv);
+	//argv0 = program
+	
+	//argv2 should equal our registration // need to process all the rgistrations we may have, array??
+	//argv3 should be --data
+	//argv4 should be json data
+	
+	$registrationType = $argv[2];
+	$data =  $argv[4];
+	
+	logEntry("PROCESSING CALLBACK");
+	$clearMessage=FALSE;
+	
+	switch ($registrationType)
+	{
+		case "media":
+			if($argv[3] == "--data")
+			{
+				$data=trim($data);
+				logEntry("DATA: ".$data);
+				$obj = json_decode($data);
+	
+				$type = $obj->{'type'};
+	
+				switch ($type) {
+						
+					case "sequence":
+	
+						//$sequenceName = ;
+						processSequenceName($obj->{'Sequence'});
+							
+						break;
+					case "media":
+							
+						logEntry("We do not understand type media at this time");
+							
+						exit(0);
+	
+						break;
+	
+					default:
+						logEntry("We do not understand: type: ".$obj->{'type'}. " at this time");
+						exit(0);
+						break;
+	
+				}
+	
+	
+			}
+	
+			break;
+			exit(0);
+				
+		default:
+			exit(0);
+	
+	}
+	
+
+
+}
+?>
---
+++ plugin_setup.php
@@ -0,0 +1,126 @@
+<?php
+//$DEBUG=true;
+
+$skipJSsettings = 1;
+//include_once '/opt/fpp/www/config.php';
+include_once '/opt/fpp/www/common.php';
+
+$pluginName = "FalconSystemMonitor";
+
+$pluginUpdateFile = $settings['pluginDirectory']."/".$pluginName."/"."pluginUpdate.inc";
+
+
+include_once 'functions.inc.php';
+include_once 'commonFunctions.inc.php';
+
+include_once 'version.inc';
+
+$myPid = getmypid();
+
+$gitURL = "https://github.com/LightsOnHudson/FPP-Plugin-Falcon-System-Monitor.git";
+
+//arg0 is  the program
+//arg1 is the first argument in the registration this will be --list
+//$DEBUG=true;
+$logFile = $settings['logDirectory']."/".$pluginName.".log";
+$sequenceExtension = ".fseq";
+
+logEntry("plugin update file: ".$pluginUpdateFile);
+
+//logEntry("open log file: ".$logFile);
+
+
+
+$DEBUG = false;
+
+if(isset($_POST['updatePlugin']))
+{
+	$updateResult = updatePluginFromGitHub($gitURL, $branch="master", $pluginName);
+	
+	echo $updateResult."<br/> \n";
+}
+
+if(isset($_POST['submit']))
+{
+	
+
+	
+	
+	//$ENABLED=$_POST["ENABLED"];
+
+	//	echo "Writring config fie <br/> \n";
+
+
+//	WriteSettingToFile("PROJ_PASSWORD",urlencode($_POST["PROJ_PASSWORD"]),$pluginName);
+
+	
+
+} 
+
+
+	
+	
+	//$ENABLED = ReadSettingFromFile("ENABLED",$pluginName);
+	$ENABLED = urldecode($pluginSettings['ENABLED']);
+	
+	
+?>
+
+<html>
+<head>
+</head>
+
+<div id="plugin" class="settings">
+<fieldset>
+<legend>Falcon System Monitor Support/Install Instructions</legend>
+
+<p>Known Issues:
+<ul>
+<li>NONE</li>
+</ul>
+
+
+<p>Configuration:
+<ul>
+<li></li>
+</ul>
+
+<form method="post" action="http://<? echo $_SERVER['SERVER_ADDR'].":".$_SERVER['SERVER_PORT']?>/plugin.php?plugin=ProjectorControl&page=plugin_setup.php">
+<br>
+<p/>
+
+<?
+
+echo "VER: ".$VERSION;
+echo "<br/> \n";
+
+echo "ENABLE PLUGIN: ";
+
+//if($ENABLED == "on" || $ENABLED == 1) {
+//	echo "<input type=\"checkbox\" checked name=\"ENABLED\"> \n";
+	PrintSettingCheckbox($pluginName, "ENABLED", $restart = 0, $reboot = 0, "ON", "OFF", $pluginName = $pluginName, $callbackName = "");
+//} else {
+//	echo "<input type=\"checkbox\"  name=\"ENABLED\"> \n";
+//}
+echo "<p/>\n";
+
+?>
+<p/>
+<input id="submit_button" name="submit" type="submit" class="buttons" value="Save Config">
+</form>
+
+
+<p>To report a bug, please file it against the  plug-in project on Git: <? echo $gitURL;?>
+<form method="post" action="http://<? echo $_SERVER['SERVER_ADDR'].":".$_SERVER['SERVER_PORT']?>/plugin.php?plugin=".$pluginName."&page=plugin_setup.php">
+<?
+ if(file_exists($pluginUpdateFile))
+ {
+ 	//echo "updating plugin included";
+	include $pluginUpdateFile;
+}
+?>
+</form>
+</fieldset>
+</div>
+<br />
+</html>
